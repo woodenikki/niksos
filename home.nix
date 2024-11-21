6155010655b6038190c09935e9b1235b76f8f7a3
woodenikki <woodenikki@gmail.com>
2024-11-20 20:46:14 -0700

    new terminal, add yoink to system clipboard

--- home.nix
+++ home.nix
@@ -1,27 +1,30 @@
-# home.nix
-
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
+  # Set user information
   home.username = "nik";
+  home.homeDirectory = lib.mkForce "/home/nik";  # Force to avoid conflicts
   home.stateVersion = "23.11";
-  home.homeDirectory = "/home/nik";
 
+  # Current Version Missmatch (idgaf)
+  home.enableNixpkgsReleaseCheck = false;
+
+
+  # Enable Zsh and specify packages
   programs.zsh.enable = true;
-
-  home.packages = [
-    pkgs.zsh
-    pkgs.vim
-    pkgs.git
+  home.packages = with pkgs; [
+    brave
+    firefox
+    spotify
+    discord
+    kitty # similar to iterm2?
   ];
 
-  # Define any other configuration options you had in your previous file
-  # For example, shell aliases, environment variables, etc.
-
+  # Git configuration for the user
   programs.git = {
-    enable    = true;
-    userName  = "woodenikki";
+    enable = true;
+    userName = "woodenikki";
     userEmail = "woodenikki@gmail.com";
   };
-}
 
+}
