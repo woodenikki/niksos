9e6e822cbd60df0e80969e9398d4cefb426f7a7d
woodenikki <woodenikki@gmail.com>
2024-12-13 14:35:24 -0700

    Fix flake configuration

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "A simple NixOS flake";
 
   inputs = {
+    # NixOS official package source, using nixos-23.11 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # Home Manager input
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,10 +23,20 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          # Correct Home Manager configuration for user `nik`
+          # Correctly import user home configuration
           home-manager.users.nik = import ./home.nix;
         }
       ];
+    };
+
+    homeConfigurations = {
+      nik = home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux";
+        homeDirectory = "/home/nik";
+        username = "nik";
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        configuration = import ./home.nix;
+      };
     };
   };
 }
